c1141a8781d7184925ce81245b2dea43c1fc1e70
Hussein Younes <husseinyounes2015@gmail.com>
2022-02-14 00:59:11 +0300

    tested and verified to work

--- Assignment1/mrl98.py
+++ Assignment1/mrl98.py
@@ -1,7 +1,12 @@
 import numpy as np
+import warnings
+import argparse
 
 
 class DataStreamer():
+    '''
+        Simulates a stream of data
+    '''
 
     def __init__(self, stream_len, batch_size):
 
@@ -21,11 +26,15 @@ class DataStreamer():
     def __next__(self):
 
         current_idx = self.current_idx
-        if current_idx * self.batch_size >= self.stream_len:
+        if current_idx >= self.stream_len:
             raise StopIteration
 
-        self.current_idx += 1
-        return self.data[self.current_idx:self.current_idx + self.batch_size]
+        self.current_idx += self.batch_size
+        batch = self.data[current_idx: current_idx + self.batch_size]
+        return batch
+
+    def done(self):
+        return self.current_idx >= self.stream_len
 
     def generate_data(self, data_len):
         return np.random.randint(data_len, size=data_len)
@@ -33,10 +42,9 @@ class DataStreamer():
 
 class Buffer:
 
-    def __init__(self, pos=0, k=3):
+    def __init__(self, k=5):
 
         self.k = k
-        self.pos = pos
         self.data = np.empty((self.k, ))
         self.weight = 0
         self.level = 0  # the bffer's level in the tree
@@ -44,7 +52,7 @@ class Buffer:
 
     def fill(self, data):
         """fill the buffer with data, data should be of shape (k, )"""
-        self.data = data
+        self.data = np.sort(data)
         self.full = True
 
     def upgrade(self):
@@ -63,93 +71,161 @@ class MRL98():
         self.k = k
         self.C = 0  # number of COLLAPSE operation carried out
         self.W = 0  # sum of weights of the output buffers in collapse operations
-        i = 0
         self.n_empty_buffers = b
         self.full_buffers = []
 
-    def collapse_policy(self, data_stream):
+    # Verified to work
+    def run_collapse_policy(self, data_stream, phi):
         """Running the algorithm proposed in the paper and merge nodes from left to right"""
 
-        while self.n_empty_buffers > 0:
+        while not data_stream.done():
+            print(f'current streamer index: {data_stream.current_idx}')
             num_full_buffers = len(self.full_buffers)
             l = min([buffer.level for buffer in self.full_buffers]
                     ) if num_full_buffers > 0 else 0
+            # If there's exactly one empty buffer, invoke NEW and assign it level l
             if self.n_empty_buffers == 1:
-                empty_buffer = Buffer(k=3)
+                empty_buffer = Buffer(k=self.k)
                 empty_buffer.level = l
                 self.new(empty_buffer, next(data_stream))
+                self.n_empty_buffers = 0
+            # If there are at least two empty buffers, invoke NEW on each and assign level 0 to each one
             elif self.n_empty_buffers >= 2:
                 for i in range(self.n_empty_buffers):
-                    empty_buffer = Buffer(k=3)
+                    empty_buffer = Buffer(k=self.k)
                     empty_buffer.level = 0
                     self.new(empty_buffer, next(data_stream))
+                self.n_empty_buffers = 0
+            # If there are no empty buffers, invoke COLLAPSE on the set of buffers at level l,
+            # assing the output buffer, level l+1
             else:
                 buffers_to_merge = self.get_buffers_at_level(l)
-                output_buffer = self.collapse(buffers_to_merge)
-                self.full_buffers.append(output_buffer)
-                # self.empty_buffers.
+                self.collapse(buffers_to_merge, l)
+                self.n_empty_buffers = len(buffers_to_merge) - 1
+
+        # There's no more data, invoke Output
+        return self.output(self.full_buffers, phi)
 
     def get_buffers_at_level(self, level):
-        """Get the buffers at the given level"""
+        '''
+            Get the buffers at the given level.
+            NOTE: pop the buffers that needs to be merged from the list of full_buffers
+        '''
+        all_full_buffers = self.full_buffers
+        self.full_buffers = []
 
         res_buffers = []
-        for buffer in self.buffers:
+        for buffer in all_full_buffers:
             if buffer.level == level:
                 res_buffers.append(buffer)
+            else:
+                self.full_buffers.append(buffer)
         return res_buffers
 
-    def collapse(self, buffers):
+    # Verified to work
+    def collapse(self, buffers, l):
         """Collapse the given buffers into one buffer, this will leave one fill buffer (the output buffer), 
         and the rest will be empty"""
 
-        output_buffer = Buffer(buffers[0].pos, k=self.k)
+        print(f'number of buffers: {len(buffers)}')
+
+        # print(f'input buffers: {buffers}')
+
+        output_buffer = Buffer(k=self.k)
         output_data = []
         output_weight = np.sum([buffer.weight for buffer in buffers])
         output_buffer.weight = output_weight
-        current_idx = 0
-        output_idx = 0
+        current_idx, output_idx = 0, 0
         indices = [0] * len(buffers)
         num_values = output_weight * self.k
         while current_idx < num_values:
             current_items = []
             for i in range(len(buffers)):
-                item = buffers[i][indices[i]]
-                current_items.append((item, i))
+                if indices[i] < self.k:
+                    item = buffers[i][indices[i]]
+                    current_items.append((item, i))
             current_min_item = min(current_items)
-            current_idx += current_min_item[1]
+            indices[current_min_item[1]] += 1
+            current_idx += buffers[current_min_item[1]].weight
+            # print(f'current item: {current_items}, current index: {current_idx}')
 
-            if current_idx % (output_idx * output_weight + int((output_weight + 1)/2)):
-                print('hitting a desired position')
+            critical_position = (
+                output_idx * output_weight + int((output_weight + 1)/2))
+            if current_idx >= critical_position:
+                # print('hitting a desired position')
                 output_idx += 1
                 output_data.append(current_min_item[0])
         assert len(output_data) == self.k
         output_buffer.fill(np.array(output_data))
-        # mark the empty buffers as empty
-        self.fix_buffers_positions(output_buffer)
+        output_buffer.level = l + 1
+        self.full_buffers.append(output_buffer)
+        print(f'output buffer weight: {output_buffer.weight}')
         return output_buffer
 
-    def fix_buffers_positions(self, output_buffer):
-        for buffer in self.full_buffers:
-            if buffer.pos != output_buffer.pos:
-                del self.full_buffers[buffer.pos]
-                self.n_empty_buffers += 1
-        for i in range(len(self.full_buffers)):
-            self.full_buffers[i].pos = i
-
+    # Verified to work
     def new(self, buffer, data):
         """Fill the given buffer with data and assign its weight to 1"""
         buffer.fill(data)
         buffer.weight = 1
-        buffer.pos = len(self.full_buffers)
         self.full_buffers.append(buffer)
 
-    def output(self):
-        pass
+    # Verified to work
+    def output(self, buffers, phi):
+        '''
+            OUTPUT takes c >= 2 full input buffers, X_1, X_2, ... , X_c, of size k,
+            and returns a single element, corresponding to the approximate phi'-quantile
+            of the augmented dataset. The phi-quantile of the original dataset corresponds to the
+            phi'-quantile of the augmented dataset, which consists of the original elements  plus
+            the -inf and +inf elements added to the last buffer.
+            NOTE: OUTPUT is invoked on the final set of full buffers.
+        '''
+        W = np.sum([buffer.weight for buffer in buffers]
+                   )  # Sum of the input buffers' weights
+        output_element_position = np.ceil(phi * self.k * W)
+
+        current_idx = 0
+        indices = [0] * len(buffers)
+        num_values = W * self.k
+        while current_idx < num_values:
+            current_items = []
+            for i in range(len(buffers)):
+                if indices[i] < self.k:
+                    item = buffers[i][indices[i]]
+                    current_items.append((item, i))
+            current_min_item = min(current_items)
+            indices[current_min_item[1]] += 1
+            current_idx += buffers[current_min_item[1]].weight
+
+            if current_idx >= output_element_position:
+                print('reached the desired element')
+                return current_min_item[0]
+        warnings.warn("Couldn't find the desired element")
+
+def main(args):
+    data_stream = DataStreamer(args.data_size, args.batch_size)
+    mrl = MRL98(b=args.num_buffers, k=args.items_per_buffer)
+    output = mrl.run_collapse_policy(data_stream, args.phi)
+    print(f'the {args.phi}-th quantile is: {output}')
 
 
-def main():
-    streamer = DataStreamer(10, 5)
+def get_args():
+    parser = argparse.ArgumentParser(description='Process arguments')
+    parser.add_argument('-k', '--items_per_buffer', default=3, type=int,
+                        help='number of items per buffer')
+    parser.add_argument('-b', '--num_buffers', default=5, type=int,
+                        help='number of buffers')
+    parser.add_argument('--data_size', default=3000, type=int,
+                        help='number of items per buffer')
+    parser.add_argument('--batch_size', default=3, type=int,
+                        help='number of items per buffer')
+    parser.add_argument('--phi', default=0.5, type=int,
+    help='number of items per buffer')
+
+    args = parser.parse_args()
+
+    return args
 
 
 if __name__ == "__main__":
-    main()
+    args = get_args()
+    main(args)
